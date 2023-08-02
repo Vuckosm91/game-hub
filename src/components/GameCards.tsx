7ee25e29a97f34e9b134987aa53527656f5ec013
Vucko <vuckosm91@gmail.com>
2023-08-03 00:44:41 +0200

    add emojis

--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -5,6 +5,7 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedimageUrl from "../services/image-url";
 import { FaGlassMartini } from "react-icons/fa";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -21,7 +22,10 @@ const GameCards = ({ game }: Props) => {
           ></PlatformIconList>
           <CriticScore score={game.metacritic}></CriticScore>
         </HStack>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="2xl">
+          {game.name}
+          <Emoji rating={game.rating_top}></Emoji>
+        </Heading>
       </CardBody>
     </Card>
   );
