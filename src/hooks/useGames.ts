35229f3e636f21ebab298cebb69077c8d8d4e598
Vucko <vuckosm91@gmail.com>
2024-03-30 01:55:36 +0100

    implement infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,36 +1,39 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
 
-const apiClient = new APIClient<Game>("/games")
 
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: {platform: Platform}[];
-    metacritic: number;
-    rating_top: number;
-  }
-  
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: {platform: Platform}[];
+  metacritic: number;
+  rating_top: number;
+}
+
+const apiClient = new APIClient<Game>("/games")
 
 const useGames = (gameQuery: GameQuery) => 
-useQuery<FetchResponse<Game>, Error>({
+useInfiniteQuery<FetchResponse<Game>, Error>({
   queryKey: ["games", gameQuery],
-  queryFn: () => 
-    apiClient
-    .getAll({
-      params: 
-  {
+  queryFn: ({pageParam = 1}) => 
+    apiClient.getAll({
+      params: {
     genres: gameQuery.genre?.id, 
     parent_platforms: gameQuery.platform?.id,
     ordering: gameQuery.sortOrder,
-    search: gameQuery.searchText
+    search: gameQuery.searchText,
+    page: pageParam
   }
-    })
-})
+    }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
+    initialPageParam: 1
+});
 
 
 export default useGames
