221e2044c81d998b39218db9703500bf46af4bc1
Vucko <vuckosm91@gmail.com>
2024-06-05 15:49:08 +0200

    build game screeenshots

---
+++ src/hooks/useScreeenshots.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/api-client";
+import { Screenshot } from "../entities/Screenshot";
+
+const useScreenshots = (gameId: number) => {
+    const apICient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`)
+   return useQuery({
+    queryKey: ["screenshots", gameId],
+    queryFn: apICient.getAll
+})}
+
+export default useScreenshots
